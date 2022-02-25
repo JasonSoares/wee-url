f1f4b9b028dec45fdea5d47dd1332b5e3fe91648
Jason Soares <soaresjd@gmail.com>
2022-02-24 23:08:26 -0800

    chore: fix test

--- test/wee_web/controllers/page_controller_test.exs
+++ test/wee_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WeeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "wee url"
   end
 end
